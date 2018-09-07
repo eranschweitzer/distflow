9136977732041947677f3c75efd28ac53bb049b8
Eran Schweitzer <eranschweitzer@gmail.com>
2018-09-07 15:48:24 -0700

    Lossy distflow function

---
+++ distflow_lossy.m
@@ -0,0 +1,37 @@
+function v = distflow_lossy(mpc, alpha)
+
+%% operator setup
+define_constants;
+NumberOfNodes=size(mpc.bus,1);
+FromNode=mpc.branch(:,F_BUS);
+ToNode=mpc.branch(:,T_BUS);
+Branch = 1:length(FromNode);
+NumberOfBranch=length(Branch);
+F = sparse(Branch,FromNode,1,NumberOfBranch,NumberOfNodes);
+T = sparse(Branch,ToNode,1,NumberOfBranch,NumberOfNodes);
+M = F-T;
+M = M(:,2:end);
+
+r = mpc.branch(:,BR_R);
+x = mpc.branch(:,BR_X);
+
+Rline = sparse(1:NumberOfBranch, 1:NumberOfBranch,r);
+Xline = sparse(1:NumberOfBranch, 1:NumberOfBranch,x);
+TFT   = T*F';
+I     = speye(NumberOfBranch);
+
+slack_voltage = mpc.bus(mpc.bus(:,BUS_TYPE) == REF, VM);
+
+pc = mpc.bus(:,PD)/mpc.baseMVA;
+qc = mpc.bus(:,QD)/mpc.baseMVA;
+
+B    = (I-TFT)^(-1);
+tau  =(Rline*(B - I)*Rline+Xline*(B - I)*Xline)*(Rline*Rline+Xline*Xline)^(-1);
+K    = (alpha*I - (1-2*alpha)*tau);
+Kinv = K^(-1);
+
+%% solution
+Rlossy=(M\Kinv)*Rline*B*T;
+Xlossy=(M\Kinv)*Xline*B*T;
+v = [slack_voltage^2 ;slack_voltage^2+Rlossy*pc+Xlossy*qc];
+v = sqrt(v);
