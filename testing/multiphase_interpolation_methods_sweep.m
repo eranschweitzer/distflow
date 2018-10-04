cd368a6c3f3420bec48332fda3b469cd10860779
Eran Schweitzer <eranschweitzer@gmail.com>
2018-10-04 11:48:49 -0700

    renamed sweep test and added new s-based methods

--- testing/multiphase_interpolation_methods_sweep.m
+++ testing/multiphase_interpolation_methods_sweep.m
@@ -9,19 +9,15 @@ opt = struct();
 alpha_range = 0.47:0.001:0.5;
 [amin, amax] = meshgrid(alpha_range);
 %% run grid-search
-res = cell(numel(amin),2);
+res = cell(numel(amin),4);
 for k = 1:numel(amin)
     opt.alpha = [amin(k), amax(k)];
     
-    %% method 3
-    opt.alpha_method = 3;
-    r = distflow_multi(Bus, Branch, opt);
-    res{k,1} = vertcat(r.vm);
-    
-    %% method 4 (quadratic)
-    opt.alpha_method = 4;
-    r = distflow_multi(Bus, Branch, opt);
-    res{k,2} = vertcat(r.vm);
+    for mtd = [3,4,5,6]
+        opt.alpha_method = mtd;
+        r = distflow_multi(Bus, Branch, opt);
+        res{k,mtd-2} = vertcat(r.vm);
+    end
 end
 
 %% calculate error
