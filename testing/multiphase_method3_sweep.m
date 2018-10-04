83142911e9db77a29b4690b9755069f1ae28acf1
Eran Schweitzer <eranschweitzer@gmail.com>
2018-10-04 10:53:28 -0700

    udated sweep test to include method 4 (despite filename)

--- testing/multiphase_method3_sweep.m
+++ testing/multiphase_method3_sweep.m
@@ -4,16 +4,24 @@
 dssres = load('OpenDSSResults.mat');
 load('Bus.mat');
 load('Branch.mat');
-opt = struct('alpha_method', 3);
+opt = struct();
 %% 
 alpha_range = 0.47:0.001:0.5;
 [amin, amax] = meshgrid(alpha_range);
 %% run grid-search
-res = cell(numel(amin),1);
+res = cell(numel(amin),2);
 for k = 1:numel(amin)
     opt.alpha = [amin(k), amax(k)];
+    
+    %% method 3
+    opt.alpha_method = 3;
     r = distflow_multi(Bus, Branch, opt);
-    res{k} = vertcat(r.vm);
+    res{k,1} = vertcat(r.vm);
+    
+    %% method 4 (quadratic)
+    opt.alpha_method = 4;
+    r = distflow_multi(Bus, Branch, opt);
+    res{k,2} = vertcat(r.vm);
 end
 
 %% calculate error
