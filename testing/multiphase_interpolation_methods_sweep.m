a0a39b4791dba0bdd87933ac235a74188ee63399
Eran Schweitzer <eranschweitzer@gmail.com>
2018-10-09 14:34:16 -0700

    Fixed (hopefully) parallelization issues

--- testing/multiphase_interpolation_methods_sweep.m
+++ testing/multiphase_interpolation_methods_sweep.m
@@ -11,28 +11,36 @@
 %   10        alpha (per phase) interpolated quadratically based on branch downstream mva magnitude
 % based on value of impedance.
 %% load data
+clear variables;
+close all;
 dssres = load('OpenDSSResults.mat');
 load('Bus.mat');
 load('Branch.mat');
+warnstate = warning('off','all');
 %% 
 alpha_range = 0.45:0.001:0.55;
 [amin, amax] = meshgrid(alpha_range);
 %% run grid-search
 mtds = 3:10;
 res = cell(numel(amin),length(mtds));
-parpool(60)
+if isempty(gcp('nocreate'))
+    parpool(2)
+end
 parfor k = 1:numel(amin)
-		opt = struct();
+    opt = struct();
     opt.alpha = [amin(k), amax(k)];
     
+    restmp = cell(1,length(mtds));
     for kk = 1:length(mtds)
-				mtd = mtds(kk);
+		mtd = mtds(kk);
         opt.alpha_method = mtd;
         r = distflow_multi(Bus, Branch, opt);
-        res{k,kk} = vertcat(r.vm);
+        restmp{kk} = vertcat(r.vm);
     end
+    res(k,:) = restmp;
 end
 delete(gcp('nocreate'))
+warning(warnstate); %restore warnings
 %% calculate error
 err = cellfun(@(x) norm(dssres.x - x), res);
-save('./testing/mutliphase_interpolation_results.mat', 'err')
+save('./testing/mutliphase_interpolation_results.mat', 'err', 'mtds')
