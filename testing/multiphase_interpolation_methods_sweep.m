904337c617559ae9387c23605d898f98dc59acb1
Eran Schweitzer <eranschweitzer@gmail.com>
2018-10-09 14:20:13 -0700

    trying to set up a sweep for alpha methods

--- testing/multiphase_interpolation_methods_sweep.m
+++ testing/multiphase_interpolation_methods_sweep.m
@@ -1,24 +1,38 @@
-% exploring multiphase method 3: range of alpha linearlizy interpolated
+% exploring multiphase methods for approximating losses
+% method #    Description
+% --------    -----------
+%    3        alpha interpolated linearly based on branch impedance
+%    4        alpha interpolated quadratically based on branch impedance
+%    5        alpha interpolated linearly based on branch downstream mva magnitude
+%    6        alpha interpolated quadratically based on branch downstream mva magnitude
+%    7        alpha (per phase) interpolated linearly based on branch impedance
+%    8        alpha (per phase) interpolated quadratically based on branch impedance
+%    9        alpha (per phase) interpolated linearly based on branch downstream mva magnitude
+%   10        alpha (per phase) interpolated quadratically based on branch downstream mva magnitude
 % based on value of impedance.
 %% load data
 dssres = load('OpenDSSResults.mat');
 load('Bus.mat');
 load('Branch.mat');
-opt = struct();
 %% 
-alpha_range = 0.47:0.001:0.5;
+alpha_range = 0.45:0.001:0.55;
 [amin, amax] = meshgrid(alpha_range);
 %% run grid-search
-res = cell(numel(amin),4);
-for k = 1:numel(amin)
+mtds = 3:10;
+res = cell(numel(amin),length(mtds));
+parpool(60)
+parfor k = 1:numel(amin)
+		opt = struct();
     opt.alpha = [amin(k), amax(k)];
     
-    for mtd = [3,4,5,6]
+    for kk = 1:length(mtds)
+				mtd = mtds(kk);
         opt.alpha_method = mtd;
         r = distflow_multi(Bus, Branch, opt);
-        res{k,mtd-2} = vertcat(r.vm);
+        res{k,kk} = vertcat(r.vm);
     end
 end
-
+delete(gcp('nocreate'))
 %% calculate error
 err = cellfun(@(x) norm(dssres.x - x), res);
+save('./testing/mutliphase_interpolation_results.mat', 'err')
