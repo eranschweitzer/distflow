0b109a02d807776194e518664aa77917c6936884
Eran Schweitzer <eranschweitzer@gmail.com>
2018-10-09 18:09:38 -0700

    added warm start solution to sweep

--- testing/multiphase_interpolation_methods_sweep.m
+++ testing/multiphase_interpolation_methods_sweep.m
@@ -18,29 +18,39 @@ load('Bus.mat');
 load('Branch.mat');
 warnstate = warning('off','all');
 %% 
-alpha_range = 0.45:0.001:0.55;
+alpha_range = 0.40:0.001:0.6;
 [amin, amax] = meshgrid(alpha_range);
 %% run grid-search
 mtds = 3:10;
-res = cell(numel(amin),length(mtds));
+res = cell(numel(amin),2*length(mtds));
 if isempty(gcp('nocreate'))
-    parpool(2)
+    parpool(60)
 end
 parfor k = 1:numel(amin)
-    opt = struct();
-    opt.alpha = [amin(k), amax(k)];
     
-    restmp = cell(1,length(mtds));
+    restmp = cell(1,2*length(mtds));
     for kk = 1:length(mtds)
-		mtd = mtds(kk);
+				mtd = mtds(kk);
+
+    		opt = struct('suppress_warnings', 1);
+    		opt.alpha = [amin(k), amax(k)];
         opt.alpha_method = mtd;
         r = distflow_multi(Bus, Branch, opt);
         restmp{kk} = vertcat(r.vm);
+				
+				opt.bustmpopt = opt;
+				opt.calcmu = 1;
+				opt.alpha = 0.5;
+				opt.alpha_method=1;
+				opt.gamma_method=2;
+        r = distflow_multi(Bus, Branch, opt);
+				restmp{kk + length(mtds)} = vertcat(r.vm);
     end
     res(k,:) = restmp;
 end
 delete(gcp('nocreate'))
 warning(warnstate); %restore warnings
 %% calculate error
-err = cellfun(@(x) norm(dssres.x - x), res);
-save('./testing/mutliphase_interpolation_results.mat', 'err', 'mtds')
+err.norm = cellfun(@(x) norm(dssres.x - x), res);
+err.max  = cellfun(@(x) max(abs(dssres.x-x)), res);
+save('./testing/mutliphase_interpolation_results.mat', 'err', 'mtds', 'amin', 'amax')
