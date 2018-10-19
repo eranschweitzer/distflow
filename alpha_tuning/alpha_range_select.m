98e7322cee4f172eb9fb3915bbc08617b460376c
Eran Schweitzer <eranschweitzer@gmail.com>
2018-10-18 20:13:05 -0700

    Touching up alpha range selection script

--- alpha_tuning/alpha_range_select.m
+++ alpha_tuning/alpha_range_select.m
@@ -8,18 +8,13 @@ close all;
 chng_error = {'alphaest_opt_with_mtds_10to200.mat',...
               'alphaest_opt_with_mtds_210to400.mat'};
 
-fnames = {'alphaest_opt_with_mtds_10to200.mat',...
-          'alphaest_opt_with_mtds_210to400.mat',...
-          'alphaest_opt_with_mtds_410to450.mat',...
-          'alphaest_opt_with_mtds_460to500.mat',...
-          'alphaest_opt_with_mtds_510to550.mat',...
-          'alphaest_opt_with_mtds_560to600.mat'};
+fnames = dir('alphaest_opt_with_mtds*.mat');
 
 %% collect data
 epsilon = struct('total', 0);
 
-for f = fnames
-	fprintf('loading %s.\n', f{:})
+for f = {fnames.name}
+	fprintf('Loading %s.\n', f{:})
 	data = load(f{:});
 	if ~exist('alpha_range','var')
 		alpha_range = data.alpha_range;
@@ -35,24 +30,32 @@ for f = fnames
 			error('methods in file %s different from already loaded set.', f{:})
 		end
 	end
-	%%%% per size
+
+	fprintf('\tCalculating per size and total errors\n')
 	for k = 1:length(data.feeder_sizes)
+		%%%% per size
 		fz = data.feeder_sizes(k);
-		tmp = sum(cat(3, data{k}{:}), 3);
+		if strcmp(f{:}, chng_error{1}) && (fz == 30)
+			continue
+		end
+		tmp = sum(cat(3, data.errs{k}{:}), 3);
 		if ismember(f{:}, chng_error)
+			fprintf('\tCorrecting error normalization by multiplying by sqrt(%d-1)\n', fz)
 			tmp = tmp*sqrt(fz-1);
 		end
 		epsilon.(['fz' num2str(fz)]) = tmp;
+
+		%%%% total
+		epsilon.total = epsilon.total + tmp;
 	end
 	
-	%%%% total
-	epsilon.total = epsilon.total + tmp;
 end
 
 %% calculate minimums
+fprintf('Saving minimum errors and corresponding ranges')
 results.tau = tau;
 results.alpha_range = alpha_range;
-results.n = zeros(length(fieldnames(epsilon)), 1);
+results.n = zeros(length(fieldnames(epsilon)) - 1, 1);
 results.amin = zeros(length(results.n), length(tau));
 results.amax = zeros(length(results.n), length(tau));
 results.err    = zeros(length(results.n), length(tau));
@@ -60,6 +63,8 @@ results.err    = zeros(length(results.n), length(tau));
 
 ptr = 1;
 for f = fieldnames(epsilon).'
+	% find row vector of minimums, each entry correspnds to one of the methods
+	% and the index is to the particular alpha_min alpha_max pair.
 	[v, I] = min(epsilon.(f{:}), [], 1);
 	alpha_min = amin(I);
 	alpha_max = amax(I);
